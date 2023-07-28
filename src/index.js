41783fc7346d15410569bb2bd8adebd7ed64267a
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-07-27 20:34:26 -0500

    filled header and foooter

--- src/index.js
+++ src/index.js
@@ -49,19 +49,26 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!!!!</h1>
+      <Header />
       <Pizza />
       <Pizza />
       <Pizza />
+      <Footer />
     </div>
   );
 }
 
-function Header() {}
+function Header() {
+  return <h1>React Pizza Co.</h1>;
+}
 
 function Menu() {}
 
-function Footer() {}
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We are currently open!</footer>
+  );
+}
 
 // component as function
 // 1) function name must be start with capital letter
