f644608cb0cb05fd596002a2032622bde81b43b8
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-07-30 17:10:17 -0500

    pass props

--- src/index.js
+++ src/index.js
@@ -71,10 +71,38 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
+  );
+}
+
+// component as function
+// 1) function name must be start with capital letter
+// 2) return some markup
+// never nest component declaration in another function
+// declare components in top level
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
   );
 }
 
@@ -93,21 +121,6 @@ function Footer() {
       {new Date().toLocaleTimeString()} We are currently
       {isOpen ? " open!" : " close."}
     </footer>
-  );
-}
-
-// component as function
-// 1) function name must be start with capital letter
-// 2) return some markup
-// never nest component declaration in another function
-// declare components in top level
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
   );
 }
 
